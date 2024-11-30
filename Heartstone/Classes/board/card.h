0b825deba91561dc69e42bf6163d2c57c367221d
WangZhaohui <singlying@outlook.com>
2024-11-30 23:28:10 +0800

    实现卡牌类-存档点

--- Heartstone/Classes/board/card.h
+++ Heartstone/Classes/board/card.h
@@ -1,37 +1,45 @@
 #pragma once
-
+#include <sstream>
+#include <fstream>
+#include <string>
 #include "cocos2d.h"
 
 class Card : public cocos2d::Sprite {
 public:
-    // 构造函数
-    Card(const std::string& name, const std::string& description, int cost) : name(name), description(description), cost(cost) {}
-
-    // 获取和设置名称
-    const std::string& getName() const { return name; }
-
     // 获取和设置描述
-    std::string getDescription() const { return description; }
+    virtual const std::string getDescription() = 0;
+    virtual const std::string getName() = 0;
 
     // 获取和设置费用
     int getCost() const { return cost; }
 
-    // 使用卡牌
-    virtual void use() = 0; // 抽象方法，子类必须实现
-
-    // 创建一个卡牌对象
-    static Card* create(const char* filename) {
-        auto sprite = new Card("Card", "This is a card", 0); // 默认名称、描述和费用
-        if (sprite && sprite->initWithFile(filename)) {
-            sprite->autorelease();
-            return sprite;
-        }
-        delete sprite;
-        return nullptr;
-    }
+    virtual void play() = 0;
 
 private:
-    std::string name; // 名称
-    std::string description; // 描述
+    std::string ID;
     int cost; // 费用
+};
+
+enum mState {
+    Taunt,  // 嘲讽
+    Charge, // 冲锋
+    Stealth,// 潜行
+    Frozen,// 冰冻
+    Windfury,// 风怒
+    lifesteal,// 吸血
+    divineshield// 圣盾
+};
+
+class MinionCard : public Card {
+public:
+    int attack;
+    int health;
+    int maxhealth;
+    bool Taunt;   
+    bool Charge;  
+    bool Stealth; 
+    bool Frozen; 
+    bool Windfury; 
+    bool lifesteal; 
+    bool divineshield; 
 };
