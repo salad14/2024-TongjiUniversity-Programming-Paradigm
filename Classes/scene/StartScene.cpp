8150d02e2a2095b3373334312a06aa592d5cf4a9
Harley Lee <3474541266@qq.com>
2024-12-07 23:47:03 +0800

    关于AudioPlayer.h头文件的特别处理 其初值及函数定义在StartScene.cpp文件中，在其他场景中只需引用此头文件，并调用audioPlayer()即可 以便在游戏开始设置音量，避免后续出现重定义错误 及 音量不统一问题

--- Classes/scene/StartScene.cpp
+++ Classes/scene/StartScene.cpp
@@ -10,6 +10,8 @@
 #include "proj.win32/Alluse.h"
 #include "proj.win32/AudioPlayer.h"
 
+ // 命名空间
+USING_NS_CC;
 // Print useful error message instead of segfaulting when files are not there.
 static void problemLoading(const char* filename)
 {
@@ -17,8 +19,27 @@ static void problemLoading(const char* filename)
     printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
 }
 
- // 命名空间
-USING_NS_CC;
+// 初始的音频引擎设置
+int backgroundMusicSign = DEFAULT_MUSIC_SIGN;
+int soundEffectSign = DEFAULT_MUSIC_SIGN;
+float backgroundMusicVolumn = DEFAULT_MUSIC_VOLUMN;
+float soundEffectVolumn = DEFAULT_MUSIC_VOLUMN;
+
+// 音频引擎方法
+void audioPlayer(const std::string& audioPath, bool isLoop)
+{
+    if (isLoop) {
+        if (backgroundMusicSign != DEFAULT_MUSIC_SIGN) {
+            cocos2d::experimental::AudioEngine::stop(backgroundMusicSign);
+        }
+        backgroundMusicSign = cocos2d::experimental::AudioEngine::play2d(audioPath, isLoop);
+        cocos2d::experimental::AudioEngine::setVolume(backgroundMusicSign, backgroundMusicVolumn);
+    }
+    else {
+        soundEffectSign = cocos2d::experimental::AudioEngine::play2d(audioPath, isLoop);
+        cocos2d::experimental::AudioEngine::setVolume(soundEffectSign, soundEffectVolumn);
+    }
+}
 
 // 创建场景
 Scene* StartScene::createScene()
